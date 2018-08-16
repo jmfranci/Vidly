285b8440caebebf008a6336e567622e3ab0744a0
Jose Francisco <junior.poster@gmail.com>
2018-08-16 01:40:34 -0400

    Downgraded Mongoose. Removed intermediate functions. Added function to validate Ids. All Crud operations working well

--- routes/genres.js
+++ routes/genres.js
@@ -9,39 +9,9 @@ const genreSchema = new mongoose.Schema({
 
 const Genre = mongoose.model('Genre', genreSchema);
 
-async function createGenre(genreObject){
-	const genre = new Genre(genreObject);
-
-	try{
-		const result = await genre.save();
-		return result;
-	}catch (ex) {
-		console.log(ex.message);
-		return;
-	}
-}
-
-async function getAllGenres(){
-	return await Genre.find().select({ genre: 1});
-}
-
-async function getGenreById(id){
-	return await Genre.findById(id);
-}
-
-async function removeGenre(id) {
-	return await Genre.deleteOne({ _id: id});
-}
-
-async function updateGenre(id, genreObject){
-	const genre = await Genre.findById(id);
-	console.log(genre);
-	if(!genre) return "Genre not found";
-
-	genre.set(genreObject);
-
-	const result = await genre.save();
-	return result;
+function isMongoIdValid(id){
+	if (id.match(/^[0-9a-fA-F]{24}$/)) return true;
+	else return false;
 }
 
 function validateGenre(genre) {
@@ -52,31 +22,28 @@ function validateGenre(genre) {
 }
 
 router.get('/', async (req,res) => {
-	try{
-		const result = await getAllGenres();
-		res.send(result);
-	}catch (ex){
-		res.send("An error has occurred: " + ex.message);
-	}
+	const genres = await Genre.find().sort('genre');
+	res.send(genres);
 });
 
 router.get('/:id', async (req, res) => {
-	const genreId = req.params.id;
-	try{
-		const result = await getGenreById(genreId);
-		if (!result) res.status(404).send('Genre not found');
-		else res.send(result);
-	}catch (ex){
-		res.send("An error has occurred: " + ex.message);
-	}
+	if (!isMongoIdValid(req.params.id)) 
+		return res.status(404).send('Invalid Id');
+
+	const genre = await Genre.findById(req.params.id);
+
+	if (!genre) return res.status(404).send('Genre not found');
+	
+	res.send (genre);
 });
 
 router.post('/', async (req, res) => {
 	const { error } = validateGenre(req.body); 
    	if (error) return res.status(400).send(error.details[0].message);
 	try{
-		const result = await createGenre(req.body);
-		res.send(result);
+		let genre = new Genre(req.body);
+		genre = await genre.save();
+		res.send(genre);
 	}catch (ex){
 		res.send("An error has occurred: " + ex.message);
 	}
@@ -85,22 +52,27 @@ router.post('/', async (req, res) => {
 
 
  router.put('/:id', async (req, res) => {
- 	const { error } = validateGenre(req.body); 
+ 	if (!isMongoIdValid(req.params.id)) 
+		return res.status(404).send('Invalid Id');
+	const { error } = validateGenre(req.body); 
    	if (error) return res.status(400).send(error.details[0].message);
 	try{
-		const result = await updateGenre(req.params.id, req.body);
-		res.send(result);
+		const genre = await Genre.findByIdAndUpdate(req.params.id, req.body, {new: true});
+		if (!genre) return res.status(404).send('Genre not found');
+		res.send(genre);
 	}catch (ex){
-		res.send('An error occurred: ' + ex.message);
+		res.send("An error has occurred: " + ex.message);
 	}
 });
 
  router.delete('/:id', async (req, res) => {
- 	try{
- 		res.send( await removeGenre(req.params.id));
- 	}catch (ex) {
- 		res.send('An error occurred: ' + ex.message);
- 	}
+ 	if (!isMongoIdValid(req.params.id)) 
+		return res.status(404).send('Invalid Id');
+	const genre = await Genre.findByIdAndRemove(req.params.id);
+
+	if (!genre) return res.status(404).send('Genre not found');
+
+	res.send (genre);
 });
 
 module.exports = router;
